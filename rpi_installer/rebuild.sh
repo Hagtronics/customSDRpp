95ce60d49d116f2be93fb45da68795c42aed6d28
Hagtronics <68252415+Hagtronics@users.noreply.github.com>
2025-08-24 13:46:56 -0700

    Add rebuild program script

---
+++ rpi_installer/rebuild.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+cd build
+
+sudo mkdir -p CMakeFiles
+sudo cmake .. -DOPT_BUILD_RTL_SDR_SOURCE=ON -DOPT_BUILD_SDRPLAY_SOURCE=ON
+
+echo "Re-Building"
+sudo make -j4
+
+echo "Installing"
+sudo make install
+
+echo "Done!"
+
+### My custom SDR++ Rebuilder
