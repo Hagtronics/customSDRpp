6e1f19f12920c944779717e32e59a75c7c1f6bee
Hagtronics <68252415+Hagtronics@users.noreply.github.com>
2025-08-28 12:41:34 -0700

    Scoping issues

--- core/src/gui/main_window.h
+++ core/src/gui/main_window.h
@@ -34,7 +34,7 @@ public:
     Event<bool> onPlayStateChange;
 
     // Added for fullscreen in gflw/backends.cpp
-    bool setFullScreen = false;
+    static bool setFullScreen = false;
 
 private:
     static void vfoAddedHandler(VFOManager::VFO* vfo, void* ctx);
