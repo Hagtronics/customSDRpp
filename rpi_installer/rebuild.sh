496d4cc4ebe25f338ded7dc0cc20e10043c96c58
Hagtronics <68252415+Hagtronics@users.noreply.github.com>
2025-09-01 18:30:36 -0700

    Update rebuild.sh Add VERBOSE=1 to Make

--- rpi_installer/rebuild.sh
+++ rpi_installer/rebuild.sh
@@ -6,7 +6,7 @@ sudo mkdir -p CMakeFiles
 sudo cmake .. -DOPT_BUILD_RTL_SDR_SOURCE=ON -DOPT_BUILD_SDRPLAY_SOURCE=ON
 
 echo "Re-Building"
-sudo make -j4
+sudo make -j4 -VERBOSE=1
 
 echo "Installing"
 sudo make install
