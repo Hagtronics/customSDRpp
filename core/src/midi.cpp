46126880fc544432740e0ed7362808a44f29e2b1
Hagtronics <68252415+Hagtronics@users.noreply.github.com>
2025-09-03 15:39:03 -0700

    Compile check

--- core/src/midi.cpp
+++ core/src/midi.cpp
@@ -14,7 +14,7 @@ static volatile int currentTuneWheel = 1;
 
 // Knob positions = 0 to 127
 volatile int currentVolumeKnob = 0;
-static volatile int currentSquelchKnob = 0;
+volatile int currentSquelchKnob = 0;
 static volatile int currentRfGainKnob = 0;
 static volatile int currentIfGainKnob = 0;
 static volatile int currentPanHKnob = 0;
