10d1fd0ade4dbc0f61afff850413efb28b94f953
Hagtronics <68252415+Hagtronics@users.noreply.github.com>
2025-09-03 17:15:01 -0700

    Add volitile back

--- core/src/midi.cpp
+++ core/src/midi.cpp
@@ -13,8 +13,8 @@
 static volatile int currentTuneWheel = 1;
 
 // Knob positions = 0 to 127
-volatile int currentVolumeKnob = 0;
-volatile int currentSquelchKnob = 0;
+static volatile int currentVolumeKnob = 0;
+static volatile int currentSquelchKnob = 0;
 static volatile int currentRfGainKnob = 0;
 static volatile int currentIfGainKnob = 0;
 static volatile int currentPanHKnob = 0;
